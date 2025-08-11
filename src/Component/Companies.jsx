9a80e4284aee44e0a684c0740e4dadbf7d201555
ALi Asghar <aliasgharchannar769@gmail.com>
2025-08-11 20:25:47 +0500

    Updated UI layout

--- src/Component/Companies.jsx
+++ src/Component/Companies.jsx
@@ -6,6 +6,12 @@ const App = () => {
 <h1 className='text-3xl font-semibold  '>Hello this is the first git project   </h1>
 
 <p className='text-2xl font-thin text-red-600   '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, minus. Beatae iure quidem reprehenderit velit sapiente ipsam eius, minus libero?</p>
+   
+   <p className='text-2xl font-semibold    '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nemo nesciunt quis enim dolorum corrupti nostrum error quisquam illo ipsum. Dolores pariatur vero ipsam sapiente ratione, in similique unde eos dolor molestias! Mollitia ipsa voluptatibus laborum quisquam alias, modi dicta quod iste a nesciunt saepe numquam ab. Vero ipsum voluptas magnam inventore, perferendis iure quo magni dolorem at asperiores a totam reiciendis soluta enim, sapiente sequi repudiandae nobis labore est? Asperiores libero quibusdam sint hic praesentium ad sed laborum quasi natus explicabo nam repudiandae mollitia vitae recusandae placeat ut deserunt, molestiae dolorum dolores minus beatae repellendus autem. Similique est atque dolor temporibus placeat rem mollitia adipisci eaque officia impedit itaque, quis aliquam ab quae laudantium commodi, necessitatibus veritatis! Unde, repudiandae qui placeat similique vitae recusandae vel officia enim saepe rem harum consectetur labore voluptates, fugiat totam quaerat illo esse odio deserunt odit in dolorum possimus. Tenetur nostrum qui quos aut, laudantium incidunt. Inventore omnis qui quia maiores ut odio laudantium dolore corporis? Pariatur error laborum molestiae nulla officiis eaque fugit veniam, vero ipsum natus ut sapiente, consectetur ipsa totam perspiciatis velit quas, accusantium quis? Sequi laborum modi quaerat corrupti alias repellat accusantium, tenetur quae error dicta eligendi ex itaque optio!</p>
+   
+   <button className='py-4 px-10 bg-red-500  text-3xl font-bold   '  >
+    Hello
+   </button>
     </div>
   )
 }
