911e88318a8b010ee1cb54fdda6fc31e71ec2eeb
ALi Asghar <aliasgharchannar769@gmail.com>
2025-08-11 19:58:59 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Companies from './Component/Companies';
+
+const App = () => {
+  return (
+    <div>
+
+      <Companies/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Companies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center flex-col  w-[100%] h-100vh   '>
+<h1 className='text-3xl font-semibold  '>Hello this is the first git project   </h1>
+
+<p className='text-2xl font-thin text-red-600   '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, minus. Beatae iure quidem reprehenderit velit sapiente ipsam eius, minus libero?</p>
+    </div>
+  )
+}
+
+export default App
